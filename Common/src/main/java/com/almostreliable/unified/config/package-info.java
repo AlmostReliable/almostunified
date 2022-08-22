d134acad7cf0c77370ffebec6e4425bfa420ff4d
Relentless <relentless@rlnt.dev>
2022-08-22 22:47:18 +0200

    add missing package infos

---
+++ Common/src/main/java/com/almostreliable/unified/config/package-info.java
@@ -0,0 +1,6 @@
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
+package com.almostreliable.unified.config;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ Common/src/main/java/com/almostreliable/unified/utils/package-info.java
@@ -0,0 +1,6 @@
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
+package com.almostreliable.unified.utils;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
