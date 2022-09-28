bed00c94152f8c3e23aea1ffa6ee94e1b808480c
LLytho <main@lytho.dev>
2022-09-28 17:50:15 +0200

    Add support for amethyst imbuement

--- Common/src/main/java/com/almostreliable/unified/api/ModConstants.java
+++ Common/src/main/java/com/almostreliable/unified/api/ModConstants.java
@@ -3,6 +3,7 @@ package com.almostreliable.unified.api;
 @SuppressWarnings("SpellCheckingInspection")
 public final class ModConstants {
     public static final String IE = "immersiveengineering";
+    public static final String AMETHYST_IMBUEMENT = "amethyst_imbuement";
 
     private ModConstants() {}
 }
