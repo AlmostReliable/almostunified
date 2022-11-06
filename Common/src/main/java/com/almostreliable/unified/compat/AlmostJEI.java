fbe03e7facb24a7164c7d54b250dfbada17fc370
Relentless <relentless@rlnt.dev>
2022-11-06 23:38:16 +0100

    add jei 9 mixin and centralize method

--- Common/src/main/java/com/almostreliable/unified/compat/AlmostJEI.java
+++ Common/src/main/java/com/almostreliable/unified/compat/AlmostJEI.java
@@ -3,10 +3,15 @@ package com.almostreliable.unified.compat;
 import com.almostreliable.unified.BuildConfig;
 import com.almostreliable.unified.config.Config;
 import com.almostreliable.unified.config.UnifyConfig;
+import com.almostreliable.unified.recipe.CRTLookup;
+import com.almostreliable.unified.utils.Utils;
+import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.runtime.IJeiRuntime;
+import net.minecraft.client.renderer.Rect2i;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 
@@ -30,6 +35,23 @@ public class AlmostJEI implements IModPlugin {
         Collection<ItemStack> items = HideHelper.createHidingList(config);
         if (!items.isEmpty()) {
             jei.getIngredientManager().removeIngredientsAtRuntime(VanillaTypes.ITEM_STACK, items);
+        }
+    }
+
+    public static <R> void recipeViewerIndicator(PoseStack stack, int mX, int mY, int x, int y, IRecipeCategory<R> recipeCategory, R recipe, int recipeBorderPadding) {
+        var recipeId = recipeCategory.getRegistryName(recipe);
+        if (recipeId == null) return;
+
+        var link = CRTLookup.getLink(recipeId);
+        if (link == null) return;
+
+        var posX = x - RecipeIndicator.SIZE / 2 - recipeBorderPadding + 1;
+        var posY = y - RecipeIndicator.SIZE / 2 - recipeBorderPadding + 1;
+        var area = new Rect2i(posX, posY, 10, 10);
+        RecipeIndicator.renderIndicator(stack, area);
+        if (mX >= area.getX() && mX <= area.getX() + area.getWidth() &&
+            mY >= area.getY() && mY <= area.getY() + area.getHeight()) {
+            Utils.renderTooltip(stack, mX, mY, RecipeIndicator.constructTooltip(link));
         }
     }
 }
--- Common/src/main/java/com/almostreliable/unified/mixin/Jei10RecipeLayoutMixin.java
+++ Common/src/main/java/com/almostreliable/unified/mixin/Jei10RecipeLayoutMixin.java
@@ -1,23 +1,22 @@
 package com.almostreliable.unified.mixin;
 
-import com.almostreliable.unified.compat.RecipeIndicator;
-import com.almostreliable.unified.recipe.CRTLookup;
-import com.almostreliable.unified.utils.Utils;
+import com.almostreliable.unified.compat.AlmostJEI;
 import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.common.gui.recipes.layout.RecipeLayout;
-import net.minecraft.client.renderer.Rect2i;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+@Pseudo
 @Mixin(RecipeLayout.class)
-public abstract class JeiRecipeLayoutMixin<R> {
+public abstract class Jei10RecipeLayoutMixin<R> {
 
     @Shadow(remap = false) @Final
     private static int RECIPE_BORDER_PADDING;
@@ -28,19 +27,6 @@ public abstract class JeiRecipeLayoutMixin<R> {
 
     @Inject(method = "drawRecipe", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;pushPose()V", ordinal = 1), locals = LocalCapture.CAPTURE_FAILHARD)
     private void unified$drawRecipe(PoseStack stack, int mouseX, int mouseY, CallbackInfo ci, IDrawable background, int mX, int mY, IDrawable categoryBackground, int x, int y) {
-        var recipeId = recipeCategory.getRegistryName(recipe);
-        if (recipeId == null) return;
-
-        var link = CRTLookup.getLink(recipeId);
-        if (link == null) return;
-
-        var posX = x - RecipeIndicator.SIZE / 2 - RECIPE_BORDER_PADDING + 1;
-        var posY = y - RecipeIndicator.SIZE / 2 - RECIPE_BORDER_PADDING + 1;
-        var area = new Rect2i(posX, posY, 10, 10);
-        RecipeIndicator.renderIndicator(stack, area);
-        if (mX >= area.getX() && mX <= area.getX() + area.getWidth() &&
-            mY >= area.getY() && mY <= area.getY() + area.getHeight()) {
-            Utils.renderTooltip(stack, mX, mY, RecipeIndicator.constructTooltip(link));
-        }
+        AlmostJEI.recipeViewerIndicator(stack, mX, mY, x, y, recipeCategory, recipe, RECIPE_BORDER_PADDING);
     }
 }
