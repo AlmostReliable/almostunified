df9fc1acd04a5a869efb457d90e868b5db96c2bb
Relentless <relentless@rlnt.dev>
2022-12-05 21:27:40 +0100

    [1.19] Improve duplicate checks for implicit counts of 1 (#28)

--- Common/src/main/java/com/almostreliable/unified/utils/JsonCompare.java
+++ Common/src/main/java/com/almostreliable/unified/utils/JsonCompare.java
@@ -10,6 +10,8 @@ import java.util.*;
 
 public final class JsonCompare {
 
+    private static final Set<String> SANITIZE_KEYS = Set.of("item", "tag", "id");
+
     private JsonCompare() {}
 
     public static int compare(JsonObject first, JsonObject second, Map<String, Rule> rules) {
@@ -36,8 +38,8 @@ public final class JsonCompare {
     }
 
     @Nullable
-    public static JsonObject compareShaped(JsonObject first, JsonObject second, Collection<String> ignoredFields) {
-        if (!matches(first, second, ignoredFields)) return null;
+    public static JsonObject compareShaped(JsonObject first, JsonObject second, CompareSettings compareSettings) {
+        if (!matches(first, second, compareSettings)) return null;
 
         JsonArray firstPattern = JsonUtils.arrayOrSelf(first.get("pattern"));
         JsonArray secondPattern = JsonUtils.arrayOrSelf(second.get("pattern"));
@@ -86,27 +88,139 @@ public final class JsonCompare {
         return keyMap;
     }
 
-    public static boolean matches(JsonObject first, JsonObject second, Collection<String> ignoredProperties) {
+    public static boolean matches(JsonObject first, JsonObject second, CompareSettings compareSettings) {
+        Collection<String> ignoredFields = compareSettings.getIgnoredFields();
         List<String> firstValidKeys = first
                 .keySet()
                 .stream()
-                .filter(key -> !ignoredProperties.contains(key))
+                .filter(key -> !ignoredFields.contains(key))
                 .toList();
         List<String> secondValidKeys = second
                 .keySet()
                 .stream()
-                .filter(key -> !ignoredProperties.contains(key))
+                .filter(key -> !ignoredFields.contains(key))
                 .toList();
 
         if (firstValidKeys.size() != secondValidKeys.size()) return false;
 
         for (String firstKey : firstValidKeys) {
-            if (!first.get(firstKey).equals(second.get(firstKey))) {
+            JsonElement firstElem = first.get(firstKey);
+            JsonElement secondElem = second.get(firstKey);
+
+            // the second element can still be null although the valid keys have the same size
+            if (secondElem == null) return false;
+
+            // sanitize elements for implicit counts of 1
+            if (compareSettings.shouldSanitize && needsSanitizing(firstElem, secondElem)) {
+                firstElem = sanitize(firstElem);
+                secondElem = sanitize(secondElem);
+            }
+
+            if (!firstElem.equals(secondElem)) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    /**
+     * A check whether the given elements need to be sanitized. The purpose of this check is
+     * to save performance by skipping pairs that are not affected by sanitizing.
+     * <p>
+     * Conditions are both elements being a JSON array with the same size, both elements being
+     * a JSON object, one element being a JSON object and the other being a JSON primitive.
+     * @param firstElem the first element
+     * @param secondElem the second element
+     * @return true if the elements need to be sanitized, false otherwise
+     */
+    private static boolean needsSanitizing(JsonElement firstElem, JsonElement secondElem) {
+        return (firstElem instanceof JsonArray firstArray && secondElem instanceof JsonArray secondArray &&
+                firstArray.size() == secondArray.size()) ||
+               (firstElem instanceof JsonObject && secondElem instanceof JsonObject) ||
+               (firstElem instanceof JsonPrimitive && secondElem instanceof JsonObject) ||
+               (firstElem instanceof JsonObject && secondElem instanceof JsonPrimitive);
+    }
+
+    /**
+     * Creates a sanitized object from the given element with a count of 1 and the
+     * value from the original object under a dummy key called "au_sanitized".
+     * <p>
+     * If the element is not a string primitive, the default object is returned.
+     * @param value The value to sanitize
+     * @param defaultValue The default value to return if the element is not a string primitive
+     * @return The sanitized object or the default value
+     */
+    private static JsonElement createSanitizedObjectOrDefault(JsonElement value, JsonElement defaultValue) {
+        if (value instanceof JsonPrimitive primitive && primitive.isString()) {
+            var newObject = new JsonObject();
+            newObject.addProperty("au_sanitized", primitive.getAsString());
+            newObject.addProperty("count", 1);
+            return newObject;
+        }
+        return defaultValue;
+    }
+
+    /**
+     * Used to sanitize root level JSON elements to make them comparable when the count of 1 is implicit.
+     * <p>
+     * This transforms string primitives, JSON objects and JSON arrays to JSON objects where the
+     * count of 1 is explicitly set. The transformation is only applied to a dummy object and not to
+     * the original recipe, so it can be safely used for comparison.
+     * <p>
+     * If the object doesn't support this transformation, the original object is returned.
+     * @param element The element to sanitize
+     * @return The sanitized element or the original element if it can't be sanitized
+     */
+    @SuppressWarnings("ChainOfInstanceofChecks")
+    private static JsonElement sanitize(JsonElement element) {
+        if (element instanceof JsonArray jsonArray) {
+            JsonArray newArray = new JsonArray();
+            for (JsonElement arrayElement : jsonArray) {
+                newArray.add(sanitize(arrayElement));
+            }
+            return newArray;
+        }
+
+        if (element instanceof JsonObject jsonObject) {
+            var keySet = jsonObject.keySet();
+
+            if (keySet.stream().filter(SANITIZE_KEYS::contains).count() != 1) {
+                return element;
+            }
+
+            // if it has a count property, it needs to be 1, otherwise it's implicit 1 as well and needs sanitizing
+            if (keySet.contains("count") && JsonQuery.of(jsonObject, "count").asInt().filter(i -> i == 1).isEmpty()) {
+                return element;
+            }
+
+            var key = keySet.stream().filter(SANITIZE_KEYS::contains).findFirst().orElseThrow();
+            var sanitized = createSanitizedObjectOrDefault(jsonObject.get(key), jsonObject);
+
+            // ensure the object changed (was sanitized) and that we got a JsonObject
+            //noinspection ObjectEquality
+            if (sanitized == jsonObject || !(sanitized instanceof JsonObject sanitizedObject)) {
+                return jsonObject;
+            }
+
+            mergeRemainingProperties(jsonObject, sanitizedObject);
+            return sanitizedObject;
+        }
+
+        return createSanitizedObjectOrDefault(element, element);
+    }
+
+    /**
+     * Merges remaining properties from the original object to the sanitized object.
+     * @param jsonObject The original object
+     * @param sanitizedObject The sanitized object
+     */
+    private static void mergeRemainingProperties(JsonObject jsonObject, JsonObject sanitizedObject) {
+        for (var entry : jsonObject.entrySet()) {
+            if (!SANITIZE_KEYS.contains(entry.getKey()) && !entry.getKey().equals("count")) {
+                sanitizedObject.add(entry.getKey(), entry.getValue());
+            }
+        }
     }
 
     public interface Rule {
@@ -154,9 +268,11 @@ public final class JsonCompare {
     public static class CompareSettings {
         public static final String IGNORED_FIELDS = "ignoredFields";
         public static final String RULES = "rules";
+        public static final String SHOULD_SANITIZE = "shouldSanitize";
 
         private final LinkedHashMap<String, Rule> rules = new LinkedHashMap<>();
         private final Set<String> ignoredFields = new HashSet<>();
+        private boolean shouldSanitize;
 
         public void ignoreField(String property) {
             ignoredFields.add(property);
@@ -168,6 +284,10 @@ public final class JsonCompare {
             if (old != null) {
                 throw new IllegalStateException("Multiple rule for key <" + key + "> found");
             }
+        }
+
+        public void setShouldSanitize(boolean shouldSanitize) {
+            this.shouldSanitize = shouldSanitize;
         }
 
         public Set<String> getIgnoredFields() {
@@ -187,6 +307,8 @@ public final class JsonCompare {
             });
             result.add(RULES, rulesJson);
 
+            result.addProperty(SHOULD_SANITIZE, shouldSanitize);
+
             return result;
         }
 
@@ -203,6 +325,8 @@ public final class JsonCompare {
                 };
                 addRule(e.getKey(), r);
             });
+
+            shouldSanitize = json.getAsJsonPrimitive(SHOULD_SANITIZE).getAsBoolean();
         }
 
         public Map<String, Rule> getRules() {
--- Common/src/test/java/com/almostreliable/unified/util/JsonCompareTest.java
+++ Common/src/test/java/com/almostreliable/unified/util/JsonCompareTest.java
@@ -1,8 +1,11 @@
 package com.almostreliable.unified.util;
 
+import com.almostreliable.unified.Platform;
 import com.almostreliable.unified.TestUtils;
+import com.almostreliable.unified.config.Defaults;
 import com.almostreliable.unified.utils.JsonCompare;
 import com.google.gson.JsonObject;
+import net.minecraft.resources.ResourceLocation;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -12,7 +15,10 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JsonCompareTest {
-    public static String recipe = """
+
+    private static final JsonCompare.CompareSettings DEFAULT_COMPARE_SETTINGS = getDefaultCompareSettings();
+    private static final JsonCompare.CompareSettings DEFAULT_SHAPED_COMPARE_SETTINGS = getDefaultShapedCompareSettings();
+    private static final String RECIPE = """
             {
               "type": "minecraft:smelting",
               "group": "coal",
@@ -25,10 +31,18 @@ public class JsonCompareTest {
             }
             """;
 
+    private static JsonCompare.CompareSettings getDefaultCompareSettings() {
+        return Defaults.getDefaultDuplicateRules(Platform.FORGE);
+    }
+
+    private static JsonCompare.CompareSettings getDefaultShapedCompareSettings() {
+        return Defaults.getDefaultDuplicateOverrides(Platform.FORGE).get(new ResourceLocation("crafting_shaped"));
+    }
+
     @Test
     public void simpleCompareFirst() {
-        JsonObject first = TestUtils.json(recipe, j -> j.addProperty("experience", 0.2));
-        JsonObject second = TestUtils.json(recipe); // 0.1 experience
+        JsonObject first = TestUtils.json(RECIPE, j -> j.addProperty("experience", 0.2));
+        JsonObject second = TestUtils.json(RECIPE); // 0.1 experience
 
         LinkedHashMap<String, JsonCompare.Rule> rules = new LinkedHashMap<>();
         rules.put("experience", new JsonCompare.LowerRule());
@@ -38,8 +52,8 @@ public class JsonCompareTest {
 
     @Test
     public void simpleCompareSecond() {
-        JsonObject first = TestUtils.json(recipe, j -> j.addProperty("experience", 0.05));
-        JsonObject second = TestUtils.json(recipe); // 0.1 experience
+        JsonObject first = TestUtils.json(RECIPE, j -> j.addProperty("experience", 0.05));
+        JsonObject second = TestUtils.json(RECIPE); // 0.1 experience
 
         LinkedHashMap<String, JsonCompare.Rule> rules = new LinkedHashMap<>();
         rules.put("experience", new JsonCompare.LowerRule());
@@ -49,8 +63,8 @@ public class JsonCompareTest {
 
     @Test
     public void compareHigherWins() {
-        JsonObject first = TestUtils.json(recipe, j -> j.addProperty("experience", 0.05));
-        JsonObject second = TestUtils.json(recipe); // 0.1 experience  // 0.1 experience
+        JsonObject first = TestUtils.json(RECIPE, j -> j.addProperty("experience", 0.05));
+        JsonObject second = TestUtils.json(RECIPE); // 0.1 experience  // 0.1 experience
 
         LinkedHashMap<String, JsonCompare.Rule> rules = new LinkedHashMap<>();
         rules.put("experience", new JsonCompare.HigherRule());
@@ -60,19 +74,19 @@ public class JsonCompareTest {
 
     @Test
     public void compareMulti() {
-        JsonObject a = TestUtils.json(recipe, j -> {
+        JsonObject a = TestUtils.json(RECIPE, j -> {
             j.addProperty("experience", 0.1);
             j.addProperty("cookingtime", 100);
         });
-        JsonObject b = TestUtils.json(recipe, j -> j.addProperty("experience", 0.1));
-        JsonObject c = TestUtils.json(recipe, j -> {
+        JsonObject b = TestUtils.json(RECIPE, j -> j.addProperty("experience", 0.1));
+        JsonObject c = TestUtils.json(RECIPE, j -> {
             j.addProperty("experience", 0.1);
             j.addProperty("cookingtime", 50);
         });
-        JsonObject d = TestUtils.json(recipe, j -> j.addProperty("experience", 0.2));
-        JsonObject e = TestUtils.json(recipe, j -> j.addProperty("experience", 0.2));
-        JsonObject f = TestUtils.json(recipe, j -> j.addProperty("experience", 0.1));
-        JsonObject g = TestUtils.json(recipe, j -> {
+        JsonObject d = TestUtils.json(RECIPE, j -> j.addProperty("experience", 0.2));
+        JsonObject e = TestUtils.json(RECIPE, j -> j.addProperty("experience", 0.2));
+        JsonObject f = TestUtils.json(RECIPE, j -> j.addProperty("experience", 0.1));
+        JsonObject g = TestUtils.json(RECIPE, j -> {
             j.addProperty("experience", 0.2);
             j.addProperty("cookingtime", 100);
         });
@@ -91,25 +105,27 @@ public class JsonCompareTest {
 
     @Test
     public void simpleMatch() {
-        JsonObject first = TestUtils.json(recipe);
-        JsonObject second = TestUtils.json(recipe);
-        boolean matches = JsonCompare.matches(first, second, List.of());
+        JsonObject first = TestUtils.json(RECIPE);
+        JsonObject second = TestUtils.json(RECIPE);
+        boolean matches = JsonCompare.matches(first, second, DEFAULT_COMPARE_SETTINGS);
         assertTrue(matches);
     }
 
     @Test
     public void noMatch() {
-        JsonObject first = TestUtils.json(recipe, j -> j.addProperty("experience", 100));
-        JsonObject second = TestUtils.json(recipe);
-        boolean matches = JsonCompare.matches(first, second, List.of());
+        JsonObject first = TestUtils.json(RECIPE, j -> j.addProperty("experience", 100));
+        JsonObject second = TestUtils.json(RECIPE);
+        boolean matches = JsonCompare.matches(first, second, new JsonCompare.CompareSettings());
         assertFalse(matches);
     }
 
     @Test
     public void matchBecauseIgnore() {
-        JsonObject first = TestUtils.json(recipe, j -> j.addProperty("experience", 100));
-        JsonObject second = TestUtils.json(recipe);
-        boolean matches = JsonCompare.matches(first, second, List.of("experience"));
+        JsonObject first = TestUtils.json(RECIPE, j -> j.addProperty("experience", 100));
+        JsonObject second = TestUtils.json(RECIPE);
+        var compareSettings = getDefaultCompareSettings();
+        compareSettings.ignoreField("experience");
+        boolean matches = JsonCompare.matches(first, second, compareSettings);
         assertTrue(matches);
     }
 
@@ -156,7 +172,7 @@ public class JsonCompareTest {
 
         JsonObject first = TestUtils.json(recipe1);
         JsonObject second = TestUtils.json(recipe2);
-        JsonObject result = JsonCompare.compareShaped(first, second, List.of("pattern", "key"));
+        JsonObject result = JsonCompare.compareShaped(first, second, DEFAULT_SHAPED_COMPARE_SETTINGS);
         assertNull(result);
     }
 
@@ -200,7 +216,86 @@ public class JsonCompareTest {
 
         JsonObject first = TestUtils.json(recipe1);
         JsonObject second = TestUtils.json(recipe2);
-        JsonObject result = JsonCompare.compareShaped(first, second, List.of("pattern", "key"));
+        JsonObject result = JsonCompare.compareShaped(first, second, DEFAULT_SHAPED_COMPARE_SETTINGS);
         assertEquals(first, result);
+    }
+
+    @Test
+    public void sanitizeImplicitCount() {
+        String recipe1 = """
+                {
+                  "type": "minecraft:crafting_shaped",
+                  "pattern": [
+                    "iii",
+                    "iii",
+                    "iii"
+                  ],
+                  "key": {
+                    "i": {
+                      "tag": "forge:raw_materials/iron"
+                    }
+                  },
+                  "result": "minecraft:iron_ingot"
+                }
+                """;
+        String recipe2 = """
+                {
+                  "type": "minecraft:crafting_shaped",
+                  "pattern": [
+                    "iii",
+                    "iii",
+                    "iii"
+                  ],
+                  "key": {
+                    "i": {
+                      "tag": "forge:raw_materials/iron"
+                    }
+                  },
+                  "result": {
+                    "item": "minecraft:iron_ingot",
+                    "count": 1
+                  }
+                }
+                """;
+
+        JsonObject first = TestUtils.json(recipe1);
+        JsonObject second = TestUtils.json(recipe2);
+        var compareSettings = getDefaultShapedCompareSettings();
+        compareSettings.setShouldSanitize(true);
+        JsonObject result = JsonCompare.compareShaped(first, second, compareSettings);
+        assertEquals(first, result);
+    }
+
+    @Test
+    public void sanitizeImplicitCountNested() {
+        String recipe1 = """
+                {
+                  "type": "create:crushing",
+                  "ingredients": [{ "tag": "forge:raw_materials/lead" }],
+                  "processingTime": 400,
+                  "results": [
+                    { "item": "emendatusenigmatica:crushed_lead_ore" },
+                    { "chance": 0.75, "item": "create:experience_nugget" }
+                  ]
+                }
+                """;
+        String recipe2 = """
+                {
+                  "type": "create:crushing",
+                  "ingredients": [{ "tag": "forge:raw_materials/lead" }],
+                  "processingTime": 400,
+                  "results": [
+                    { "count": 1, "item": "emendatusenigmatica:crushed_lead_ore" },
+                    { "chance": 0.75, "count": 1, "item": "create:experience_nugget" }
+                  ]
+                }
+                """;
+
+        JsonObject first = TestUtils.json(recipe1);
+        JsonObject second = TestUtils.json(recipe2);
+        var compareSettings = getDefaultCompareSettings();
+        compareSettings.setShouldSanitize(true);
+        boolean result = JsonCompare.matches(first, second, compareSettings);
+        assertTrue(result);
     }
 }
