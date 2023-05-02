7f8b6001d7c3c3217e05db5ee68ff58083c287cb
Relentless <relentless@rlnt.dev>
2023-05-02 11:02:14 +0200

    add input items support and refactor

--- Forge/src/main/java/com/almostreliable/unified/compat/ArsNouveauRecipeUnifier.java
+++ Forge/src/main/java/com/almostreliable/unified/compat/ArsNouveauRecipeUnifier.java
@@ -1,30 +1,40 @@
 package com.almostreliable.unified.compat;
 
 import com.almostreliable.unified.api.recipe.RecipeConstants;
+import com.almostreliable.unified.api.recipe.RecipeContext;
 import com.almostreliable.unified.api.recipe.RecipeUnifier;
 import com.almostreliable.unified.api.recipe.RecipeUnifierBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
+import javax.annotation.Nullable;
+
 public class ArsNouveauRecipeUnifier implements RecipeUnifier {
+
     @Override
     public void collectUnifier(RecipeUnifierBuilder builder) {
-        // pedestalItems is a JsonArray with a JsonObject of {"item":Ingredient}
-        builder.put(RecipeConstants.PEDESTAL_ITEMS, JsonArray.class, (json, ctx) -> {
-            for (int i = 0; i < json.size(); i++) {
-                JsonElement entry = json.get(i);
-                JsonElement result = null;
-                if (entry instanceof JsonObject jsonEntry) {
-                    result = ctx.createIngredientReplacement(jsonEntry.get(RecipeConstants.ITEM));
-                }
-                if (result != null) {
-                    JsonObject resultObj = new JsonObject();
-                    resultObj.add(RecipeConstants.ITEM, result);
-                    json.set(i, resultObj);
-                }
-            }
-            return json;
-        });
+        // imbuement, enchanting apparatus, enchantment, reactive enchantment, spell write, armor upgrade
+        builder.forEachObject(RecipeConstants.PEDESTAL_ITEMS, this::createIngredientReplacement);
+        // glyph
+        builder.forEachObject(RecipeConstants.INPUT_ITEMS, this::createIngredientReplacement);
+
+        // enchanting apparatus
         builder.put(RecipeConstants.REAGENT, (json, ctx) -> ctx.createIngredientReplacement(json));
+    }
+
+    /**
+     * Handles ingredient replacements for JSON arrays with nested item JSON objects.
+     *
+     * @param json The JSON object to apply the replacement on.
+     * @param ctx  The recipe context.
+     * @return The JSON object with the replacement applied, or {@code null} if there is no replacement.
+     */
+    @Nullable
+    private JsonObject createIngredientReplacement(JsonObject json, RecipeContext ctx) {
+        var replacement = ctx.createIngredientReplacement(json.get(RecipeConstants.ITEM));
+        if (replacement instanceof JsonObject item) {
+            json.add(RecipeConstants.ITEM, item);
+            return json;
+        }
+        return null;
     }
 }
