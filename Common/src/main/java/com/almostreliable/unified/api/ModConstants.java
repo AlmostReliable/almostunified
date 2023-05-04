1a8f92f83a4256e23d860fff3725cb76e7882215
Relentless <relentless@rlnt.dev>
2023-05-04 20:44:22 +0200

    port 1.19.2 changes

--- Common/src/main/java/com/almostreliable/unified/api/ModConstants.java
+++ Common/src/main/java/com/almostreliable/unified/api/ModConstants.java
@@ -9,7 +9,10 @@ public final class ModConstants {
     // custom unify handlers
     public static final String AD_ASTRA = "ad_astra";
     public static final String AMETHYST_IMBUEMENT = "amethyst_imbuement";
+    public static final String ARS_CREO = "ars_creo";
+    public static final String ARS_ELEMENTAL = "ars_elemental";
     public static final String ARS_NOUVEAU = "ars_nouveau";
+    public static final String ARS_SCALAES = "ars_scalaes";
     public static final String IMMERSIVE_ENGINEERING = "immersiveengineering";
     public static final String MEKANISM = "mekanism";
     public static final String MODERN_INDUSTRIALIZATION = "modern_industrialization";
