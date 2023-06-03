a4b59204400223aee4e2395952c83edd5126d27d
AlexSa1000 <56562013+AlexSa1000@users.noreply.github.com>
2023-06-03 16:52:02 +0100

    Update Defaults.java
    Compatibility with ported cucumber version for fabric

--- Common/src/main/java/com/almostreliable/unified/config/Defaults.java
+++ Common/src/main/java/com/almostreliable/unified/config/Defaults.java
@@ -132,7 +132,7 @@ public final class Defaults {
     public static List<String> getIgnoredRecipeTypes(Platform platform) {
         return switch (platform) {
             case FORGE -> List.of("cucumber:shaped_tag");
-            case FABRIC -> List.of();
+            case FABRIC -> List.of("cucumber:shaped_tag");
         };
     }
 
