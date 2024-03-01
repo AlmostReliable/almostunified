a81bc4c34bc4ca2cafdade3acc65ab0d4f47a4a4
rlnt <relentless@rlnt.dev>
2024-03-01 23:30:04 +0100

    rework the unifier

--- Common/src/main/java/com/almostreliable/unified/api/recipe/RecipeConstants.java
+++ Common/src/main/java/com/almostreliable/unified/api/recipe/RecipeConstants.java
@@ -61,6 +61,10 @@ public final class RecipeConstants {
     public static final String EXTRA_DROP = "extra_drop";
     public static final String BATCH = "batch";
     public static final String RESULT_ITEM = "result_item";
+    public static final String MOLD = "mold";
+    public static final String PRIMARY_INGREDIENT = "primary_ingredient";
+    public static final String SECONDARY_INPUT = "secondary_input";
+    public static final String BASE_INGREDIENT = "base_ingredient";
 
     private RecipeConstants() {}
 }
--- Forge/src/main/java/com/almostreliable/unified/compat/TerraFirmaCraftRecipeUnifier.java
+++ Forge/src/main/java/com/almostreliable/unified/compat/TerraFirmaCraftRecipeUnifier.java
@@ -5,50 +5,70 @@ import com.almostreliable.unified.api.recipe.RecipeContext;
 import com.almostreliable.unified.api.recipe.RecipeUnifier;
 import com.almostreliable.unified.api.recipe.RecipeUnifierBuilder;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
 public class TerraFirmaCraftRecipeUnifier implements RecipeUnifier {
 
+    public static final String ITEM_OUTPUT = "item_output";
+
     @Override
     public void collectUnifier(RecipeUnifierBuilder builder) {
-        // regular ingredient
         List.of(
-                RecipeConstants.FIRST_INPUT,
-                RecipeConstants.SECOND_INPUT,
+                // barrel_sealed, vat
                 RecipeConstants.INPUT_ITEM,
-                RecipeConstants.BATCH
-        ).forEach(key -> builder.put(key, (json, ctx) -> ctx.createIngredientReplacement(json)));
+                // blast_furnace, bloomery
+                RecipeConstants.CATALYST,
+                // casting
+                RecipeConstants.MOLD,
+                // advanced shapeless crafting
+                RecipeConstants.PRIMARY_INGREDIENT,
+                // glassworking
+                RecipeConstants.BATCH,
+                // power_loom
+                RecipeConstants.INPUTS,
+                RecipeConstants.SECONDARY_INPUT,
+                // welding
+                RecipeConstants.FIRST_INPUT,
+                RecipeConstants.SECOND_INPUT
+        ).forEach(key -> builder.put(key, (json, ctx) -> ctx.createIngredientReplacement(
+                json,
+                RecipeConstants.VALUE,
+                RecipeConstants.BASE,
+                RecipeConstants.INGREDIENT,
+                RecipeConstants.BASE_INGREDIENT
+        )));
 
-        // array of regular ingredients
-        builder.forEachObject(RecipeConstants.EXTRA_PRODUCTS, this::createIngredientReplacement);
-
-        // output keys that may contain "stack"
         List.of(
+                // beamhouse, advanced shapeless crafting, fleshing_machine, grist_mill, thresher
                 RecipeConstants.RESULT,
+                // filling
+                RecipeConstants.RESULTS,
+                // barrel_sealed, mixing_bowl, vat
                 RecipeConstants.OUTPUT_ITEM,
+                // chisel, scraping
                 RecipeConstants.EXTRA_DROP,
-                RecipeConstants.RESULT_ITEM
+                // heating, oven
+                RecipeConstants.RESULT_ITEM,
+                // pot
+                ITEM_OUTPUT,
+                // power_loom, thresher
+                RecipeConstants.SECONDARIES,
+                // extra products shapeless crafting
+                RecipeConstants.EXTRA_PRODUCTS
         ).forEach(key -> builder.put(key, this::createResultReplacement));
     }
 
     @Nullable
-    private JsonElement createResultReplacement(@Nullable JsonElement element, RecipeContext ctx) {
-        if (element instanceof JsonObject json && json.has(RecipeConstants.STACK)) {
-            return ctx.createResultReplacement(json.get(RecipeConstants.STACK));
-        }
-        return ctx.createResultReplacement(element);
-    }
-
-    @Nullable
-    private JsonObject createIngredientReplacement(JsonObject json, RecipeContext ctx) {
-        var replacement = ctx.createIngredientReplacement(json.get(RecipeConstants.ITEM));
-        if (replacement instanceof JsonObject item) {
-            json.add(RecipeConstants.ITEM, item);
-            return json;
-        }
-        return null;
+    private JsonElement createResultReplacement(JsonElement json, RecipeContext ctx) {
+        return ctx.createResultReplacement(
+                json,
+                false,
+                RecipeConstants.ITEM,
+                RecipeConstants.STACK,
+                RecipeConstants.OUTPUT,
+                RecipeConstants.BASE_INGREDIENT
+        );
     }
 }
