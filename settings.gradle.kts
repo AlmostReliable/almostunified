63c54c317e3dd8ba6e1f7f4c95ba7a2384d450fa
rlnt <relentless@rlnt.dev>
2024-06-23 15:22:21 +0200

    temporarily disable Fabric module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,4 +11,4 @@ pluginManagement {
 val modName: String by extra
 val minecraftVersion: String by extra
 rootProject.name = "$modName-$minecraftVersion"
-include("Common", "Fabric", "NeoForge")
+include("Common", "NeoForge")
