627ec47bb76b7afa5059d7888a1c84c0101865cc
rlnt <relentless@rlnt.dev>
2025-04-27 18:08:08 +0200

    fix output comparison not using the unified output

--- Common/src/main/java/com/almostreliable/unified/unification/recipe/RecipeLink.java
+++ Common/src/main/java/com/almostreliable/unified/unification/recipe/RecipeLink.java
@@ -3,6 +3,7 @@ package com.almostreliable.unified.unification.recipe;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 
 import com.almostreliable.unified.AlmostUnifiedCommon;
 import com.almostreliable.unified.api.constant.RecipeConstants;
@@ -50,19 +51,6 @@ public final class RecipeLink implements RecipeData {
         this.originalRecipe = originalRecipe;
         this.type = type;
         this.isCraftingRecipe = type == SHAPED_RECIPE_TYPE || type == SHAPELESS_RECIPE_TYPE;
-
-        if (isCraftingRecipe) {
-            try {
-                String outputString = originalRecipe
-                    .getAsJsonObject(RecipeConstants.RESULT)
-                    .getAsJsonPrimitive(RecipeConstants.ID)
-                    .getAsString();
-                this.craftingRecipeOutput = BuiltInRegistries.ITEM.get(ResourceLocation.parse(outputString));
-            } catch (Exception e) {
-                AlmostUnifiedCommon.LOGGER.warn("Could not detect crafting recipe output for recipe '{}'.", id);
-                this.craftingRecipeOutput = null;
-            }
-        }
     }
 
     @Nullable
@@ -99,7 +87,7 @@ public final class RecipeLink implements RecipeData {
      */
     @Nullable
     public static RecipeLink compare(RecipeLink first, RecipeLink second, JsonCompare.CompareContext compareContext) {
-        if (first.isCraftingRecipe && first.craftingRecipeOutput != second.craftingRecipeOutput) {
+        if (first.isCraftingRecipe && first.getCraftingRecipeOutput() != second.getCraftingRecipeOutput()) {
             return null;
         }
 
@@ -173,6 +161,29 @@ public final class RecipeLink implements RecipeData {
         }
 
         this.unifiedRecipe = json;
+    }
+
+    @Nullable
+    public Item getCraftingRecipeOutput() {
+        if (craftingRecipeOutput == null) {
+            JsonObject recipe = unifiedRecipe == null ? originalRecipe : unifiedRecipe;
+            try {
+                String outputString = recipe
+                    .getAsJsonObject(RecipeConstants.RESULT)
+                    .getAsJsonPrimitive(RecipeConstants.ITEM)
+                    .getAsString();
+                craftingRecipeOutput = BuiltInRegistries.ITEM.get(ResourceLocation.parse(outputString));
+            } catch (Exception e) {
+                AlmostUnifiedCommon.LOGGER.warn("Could not detect crafting recipe output for recipe '{}'.", id);
+                craftingRecipeOutput = Items.AIR;
+            }
+        }
+
+        if (craftingRecipeOutput == Items.AIR) {
+            return null;
+        }
+
+        return craftingRecipeOutput;
     }
 
     @Override
