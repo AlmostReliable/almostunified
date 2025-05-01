fa03633b4318b1abda58eab67d8fc4b1e0a7f1d5
sh1ftchg <sh1ftchg@users.noreply.github.com>
2025-05-01 18:32:27 -0500

    change constants file to appropriate java constants type and update gradle to support java 24 lts

--- Common/src/main/java/com/almostreliable/unified/api/ModConstants.java
+++ Common/src/main/java/com/almostreliable/unified/api/ModConstants.java
@@ -1,33 +1,31 @@
 package com.almostreliable.unified.api;
 
 @SuppressWarnings("SpellCheckingInspection")
-public final class ModConstants {
+public interface ModConstants {
     // recipe viewers
-    public static final String JEI = "jei";
-    public static final String REI = "roughlyenoughitems";
+    String JEI = "jei";
+    String REI = "roughlyenoughitems";
 
     // custom unify handlers
-    public static final String AD_ASTRA = "ad_astra";
-    public static final String APPLIED_ENERGISTICS = "ae2";
-    public static final String ALLOY_FORGERY = "alloy_forgery";
-    public static final String AMETHYST_IMBUEMENT = "amethyst_imbuement";
-    public static final String ARS_CREO = "ars_creo";
-    public static final String ARS_ELEMENTAL = "ars_elemental";
-    public static final String ARS_NOUVEAU = "ars_nouveau";
-    public static final String ARS_SCALAES = "ars_scalaes";
-    public static final String BLOOD_MAGIC = "bloodmagic";
-    public static final String CYCLIC = "cyclic";
-    public static final String ENDER_IO = "enderio";
-    public static final String GREGTECH_MODERN = "gtceu";
-    public static final String IMMERSIVE_ENGINEERING = "immersiveengineering";
-    public static final String INTEGRATED_DYNAMICS = "integrateddynamics";
-    public static final String MEKANISM = "mekanism";
-    public static final String MODERN_INDUSTRIALIZATION = "modern_industrialization";
-    public static final String WOODENCOG = "woodencog";
-    public static final String TERRAFIRMACRAFT = "tfc";
-    public static final String ADVANCED_TFC_TECH = "advancedtfctech";
-    public static final String FIRMALIFE = "firmalife";
-    public static final String TFC_WATER_FLASKS = "waterflasks";
-
-    private ModConstants() {}
+    String AD_ASTRA = "ad_astra";
+    String APPLIED_ENERGISTICS = "ae2";
+    String ALLOY_FORGERY = "alloy_forgery";
+    String AMETHYST_IMBUEMENT = "amethyst_imbuement";
+    String ARS_CREO = "ars_creo";
+    String ARS_ELEMENTAL = "ars_elemental";
+    String ARS_NOUVEAU = "ars_nouveau";
+    String ARS_SCALAES = "ars_scalaes";
+    String BLOOD_MAGIC = "bloodmagic";
+    String CYCLIC = "cyclic";
+    String ENDER_IO = "enderio";
+    String GREGTECH_MODERN = "gtceu";
+    String IMMERSIVE_ENGINEERING = "immersiveengineering";
+    String INTEGRATED_DYNAMICS = "integrateddynamics";
+    String MEKANISM = "mekanism";
+    String MODERN_INDUSTRIALIZATION = "modern_industrialization";
+    String WOODENCOG = "woodencog";
+    String TERRAFIRMACRAFT = "tfc";
+    String ADVANCED_TFC_TECH = "advancedtfctech";
+    String FIRMALIFE = "firmalife";
+    String TFC_WATER_FLASKS = "waterflasks";
 }
