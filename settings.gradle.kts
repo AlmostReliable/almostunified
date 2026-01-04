e6524933020a089b86fa03ce52f47f9b270c2bc3
rlnt <relentless@rlnt.dev>
2026-01-04 22:31:44 +0100

    update project files to 26.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,15 @@
 pluginManagement {
     repositories {
-        maven("https://maven.architectury.dev/")
-        maven("https://maven.fabricmc.net/")
-        maven("https://maven.minecraftforge.net/")
-        maven("https://maven.neoforged.net/releases")
+        mavenLocal()
         gradlePluginPortal()
+        mavenCentral()
     }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 val modName: String by extra
 val minecraftVersion: String by extra
-rootProject.name = "$modName-$minecraftVersion"
-include("Common", "NeoForge", "Fabric")
+rootProject.name = "${modName.replace(" ", "-")}-$minecraftVersion-NeoForge"
