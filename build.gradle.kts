b03063774f6fa85ba47cab491295e7e24a2257cb
rlnt <relentless@rlnt.dev>
2026-01-04 23:48:27 +0100

    switch to official almostgradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("net.neoforged.moddev") version "2.0.137"
-    id("com.almostreliable.almostgradle") version "1.4.3"
+    id("com.almostreliable.almostgradle") version "1.5.0"
 }
 
 almostgradle.setup {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,3 @@
-pluginManagement {
-    repositories {
-        mavenLocal()
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
