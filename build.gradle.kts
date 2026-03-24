3fd2e8c9a54c5fdb684c979375a791f3b7eeec50
rlnt <relentless@rlnt.dev>
2026-03-24 17:56:33 +0100

    update MDG and AG

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("net.neoforged.moddev") version "2.0.137"
-    id("com.almostreliable.almostgradle") version "1.5.0"
+    id("net.neoforged.moddev") version "2.0.141"
+    id("com.almostreliable.almostgradle") version "1.5.2"
 }
 
 almostgradle.setup {
